80bac1548a9615d400f8d01a6fde222399fb530c
Greg Meltzer <gregory.m.meltzer@gmail.com>
2023-10-01 18:57:07 -0400

    initial commit

---
+++ test/pigeon_scoops/core_test.clj
@@ -0,0 +1,7 @@
+(ns pigeon-scoops.core-test
+  (:require [clojure.test :refer :all]
+            [pigeon-scoops.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
