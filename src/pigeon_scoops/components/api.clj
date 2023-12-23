b070a2c99d0b6f0da2e3266c353b4e36fd59a9e7
Greg Meltzer <gregory.m.meltzer@gmail.com>
2023-12-23 11:24:34 -0500

    formatting groceries and adding a broken route

--- src/pigeon_scoops/components/api.clj
+++ src/pigeon_scoops/components/api.clj
@@ -1,18 +1,24 @@
 (ns pigeon-scoops.components.api
   (:require [clojure.tools.logging :as logger]
             [pigeon-scoops.components.config-manager :as cm]
+            [pigeon-scoops.components.grocery-manager :as gm]
             [com.stuartsierra.component :as component]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [muuntaja.middleware :as mw]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.logger :as log-mw]
-            [ring.middleware.defaults :as defaults]))
+            [ring.middleware.defaults :as defaults]
+            [ring.util.response :refer [response]]))
 
-(defn app-routes [config-manager]
+(defn get-groceries-handler [grocery-manager params]
+  (fn [& _]
+    (response (apply (partial gm/get-groceries grocery-manager) (:types params)))))
+
+(defn app-routes [config-manager grocery-manager]
   (routes
-    (GET "api/v1/groceries" {} ())
-    (route/resources "/")
+    (GET "api/hello" {} (fn [_] (response "hello world")))
+    (GET "api/v1/groceries" {params :params} (get-groceries-handler grocery-manager params))
     (route/not-found "Not Found")))
 
 (defrecord Api [config-manager grocery-manager]
@@ -22,7 +28,7 @@
     (let [{::cm/keys [app-host app-port]} (::cm/app-settings config-manager)]
       (logger/info (str "Starting server on host " app-host " port: " app-port))
       (assoc this :server (run-jetty
-                            (-> (app-routes config-manager)
+                            (-> (app-routes config-manager grocery-manager)
                                 log-mw/wrap-with-logger
                                 mw/wrap-format
                                 (defaults/wrap-defaults {:params {:keywordize true
