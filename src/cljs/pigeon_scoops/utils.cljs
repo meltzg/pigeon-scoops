8927f33bd3e319603d4242f242ff8e00834e0def
Greg Meltzer <gregory.m.meltzer@gmail.com>
2023-12-28 15:45:56 -0500

    remove value caster

--- src/cljs/pigeon_scoops/utils.cljs
+++ src/cljs/pigeon_scoops/utils.cljs
@@ -9,13 +9,10 @@
 (defn drop-nth [n coll]
   (keep-indexed #(if (not= %1 n) %2) coll))
 
-(defn use-validation
-  ([initial-value valid-pred]
-   (use-validation initial-value valid-pred identity))
-  ([initial-value valid-pred value-caster]
-   (let [[value set-value!] (uix.core/use-state initial-value)
-         [valid? set-valid!] (uix.core/use-state (valid-pred initial-value))
-         on-change #(let [v (value-caster (.. % -target -value))]
-                      (set-value! v)
-                      (set-valid! (valid-pred v)))]
-     [value valid? on-change])))
+(defn use-validation [initial-value valid-pred]
+  (let [[value set-value!] (uix.core/use-state initial-value)
+        [valid? set-valid!] (uix.core/use-state (valid-pred initial-value))
+        on-change #(let [v (.. % -target -value)]
+                     (set-value! v)
+                     (set-valid! (valid-pred v)))]
+    [value valid? on-change]))
