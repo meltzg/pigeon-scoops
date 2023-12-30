4dc9867078efc7ab259301037b35266887bcdcbd
Greg Meltzer <gregory.m.meltzer@gmail.com>
2023-12-30 14:35:19 -0500

    add grocery add and update routes

--- src/clj/pigeon_scoops/components/api.clj
+++ src/clj/pigeon_scoops/components/api.clj
@@ -1,30 +1,48 @@
 (ns pigeon-scoops.components.api
   (:require [clojure.tools.logging :as logger]
-            [pigeon-scoops.components.config-manager :as cm]
-            [pigeon-scoops.components.grocery-manager :as gm]
             [com.stuartsierra.component :as component]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [muuntaja.middleware :as mw]
+            [pigeon-scoops.components.config-manager :as cm]
+            [pigeon-scoops.components.grocery-manager :as gm]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.logger :as log-mw]
-            [ring.middleware.params :refer [wrap-params]]
-            [ring.middleware.nested-params :refer [wrap-nested-params]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
-            [ring.util.response :refer [response resource-response]]))
+            [ring.middleware.nested-params :refer [wrap-nested-params]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.util.response :as resp]))
 
 (defn get-groceries-handler [grocery-manager params]
   (fn [& _]
-    (response (apply (partial gm/get-groceries grocery-manager)
-                     (map #(keyword (namespace ::gm/type) %)
-                          (if (or (nil? (:types params)) (coll? (:types params)))
-                            (:types params)
-                            [(:types params)]))))))
+    (resp/response (apply (partial gm/get-groceries grocery-manager)
+                          (map #(keyword (namespace ::gm/type) %)
+                               (if (or (nil? (:types params)) (coll? (:types params)))
+                                 (:types params)
+                                 [(:types params)]))))))
+
+(defn add-grocery-item-handler [grocery-manager update?]
+  (fn [{:keys [body-params]}]
+    (let [updated-groceries (gm/add-grocery-item grocery-manager body-params update?)]
+      (cond (nil? updated-groceries)
+            (if update?
+              (resp/not-found (str "No grocery item with type " (::gm/type body-params)))
+              (-> (str "Grocery item with type " (::gm/type body-params) " already exists")
+                  resp/bad-request
+                  (resp/status 409)))
+            (:clojure.spec.alpha/problems updated-groceries)
+            (-> updated-groceries
+                resp/bad-request
+                (resp/status 422))
+            :otherwise-success
+            (resp/response updated-groceries)))))
 
 (defn app-routes [config-manager grocery-manager]
   (routes
-    (GET "/" [] (resource-response "index.html" {:root "public"}))
+    (GET "/" {} (resp/resource-response "index.html" {:root "public"}))
     (GET "/api/v1/groceries" {params :params} (get-groceries-handler grocery-manager params))
+    (PUT "/api/v1/groceries" {} (add-grocery-item-handler grocery-manager false))
+    (PATCH "/api/v1/groceries" {} (add-grocery-item-handler grocery-manager true))
     (route/resources "/")
     (route/not-found "Not Found")))
 
