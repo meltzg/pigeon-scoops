499371414f93de3a5e3936741ecf18de0ada55b6
Greg Meltzer <gregory.m.meltzer@gmail.com>
2023-12-30 15:46:47 -0500

    refresh groceries on change

--- src/cljs/pigeon_scoops/core.cljs
+++ src/cljs/pigeon_scoops/core.cljs
@@ -2,19 +2,21 @@
   (:require [ajax.core :as a]
             [pigeon-scoops.apps.groceries :refer [grocery-list]]
             [pigeon-scoops.utils :refer [api-url]]
-            [uix.core :as uix :refer [defui $]]
+            [uix.core :as uix :refer [$ defui]]
             [uix.dom]))
 
 (defui content []
-       (let [[groceries set-groceries!] (uix/use-state nil)]
+       (let [[groceries set-groceries!] (uix/use-state nil)
+             [refresh-groceries? set-refresh-groceries!] (uix/use-state true)]
          (uix/use-effect
            (fn []
              (a/GET (str api-url "groceries")
                     {:response-format :transit
                      :keywords?       true?
                      :handler         set-groceries!}))
-           [])
-         ($ grocery-list {:groceries groceries})))
+           [refresh-groceries?])
+         ($ grocery-list {:groceries groceries
+                          :on-change #(set-refresh-groceries! (not refresh-groceries?))})))
 
 (defonce root
          (uix.dom/create-root (js/document.getElementById "root")))
