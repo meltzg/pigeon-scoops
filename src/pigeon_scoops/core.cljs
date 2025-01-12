1f387b4c093de91a6b11e38c25f4e96dcbbd889d
Gregory Meltzer <vadercows@gmail.com>
2025-01-12 09:46:58 -0500

    basic auth0 integration

--- src/pigeon_scoops/core.cljs
+++ src/pigeon_scoops/core.cljs
@@ -1,9 +1,25 @@
 (ns pigeon-scoops.core
-  (:require [uix.core :as uix :refer [defui $]]
-            [uix.dom]))
+  (:require [uix.core :refer [$ defui]]
+            [uix.dom]
+            ["@auth0/auth0-react" :refer [Auth0Provider useAuth0]]))
+
+(defui LoginButton []
+       (let [{:keys [loginWithRedirect]} (js->clj (useAuth0) :keywordize-keys true)]
+         ($ :button {:on-click loginWithRedirect}
+            "Login")))
+
+(defui LogoutButton []
+       (let [{:keys [logout]} (js->clj (useAuth0) :keywordize-keys true)]
+         ($ :button {:on-click #(logout (clj->js {:logoutParams {:returnTo (.. js/window -location -origin)}}))}
+            "Logout")))
 
 (defui app []
-       ($ :h1 "Hello, UIx!"))
+       ($ Auth0Provider {:domain               "pigeon-scoops.us.auth0.com"
+                         :client-id            "AoU9LnGWQlCbSUvjgXdHf4NZPJh0VHYD"
+                         :authorization-params (clj->js {:redirect_uri (.. js/window -location -origin)})}
+          ($ :h1 "Hello, UIx!")
+          ($ LoginButton)
+          ($ LogoutButton)))
 
 (defonce root
          (uix.dom/create-root (js/document.getElementById "root")))
