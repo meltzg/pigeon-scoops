2792effb38f3773dec13e5b931677c5206e45f53
Gregory Meltzer <vadercows@gmail.com>
2025-01-29 14:29:51 -0500

    render units

---
+++ src/pigeon_scoops/components/number_field.cljs
@@ -0,0 +1,48 @@
+(ns pigeon-scoops.components.number-field
+  (:require [uix.core :as uix :refer [$ defui]]
+            ["@base-ui-components/react/number-field" :refer [NumberField]]))
+
+(defui cursor-grow-icon []
+       ($ :svg {:width   "26"
+                :height  "14"
+                :viewBox "0 0 24 14"
+                :fill    "black"
+                :stroke  "white"
+                :xmlns   "http://www.w3.org/2000/svg"}
+          ($ :path {:d "M19.5 5.5L6.49737 5.51844V2L1 6.9999L6.5 12L6.49737 8.5L19.5 8.5V12L25 6.9999L19.5 2V5.5Z"})))
+
+(defui plus-icon []
+       ($ :svg {:width        "10"
+                :height       "10"
+                :viewBox      "0 0 10 10"
+                :fill         "none"
+                :stroke       "currentcolor"
+                :stroke-width "1.6"
+                :xmlns        "http://www.w3.org/2000/svg"}
+          ($ :path {:d "M0 5H5M10 5H5M5 5V0M5 5V10"})))
+
+(defui minus-icon []
+       ($ :svg {:width        "10"
+                :height       "10"
+                :viewBox      "0 0 10 10"
+                :fill         "none"
+                :stroke       "currentcolor"
+                :stroke-width "1.6"
+                :xmlns        "http://www.w3.org/2000/svg"}
+          ($ :path {:d "M0 5H10"})))
+
+(defui number-field [{:keys [value set-value! label]}]
+       (let [id (uix/use-id)]
+         ($ NumberField.Root {:id              id
+                              :value           value
+                              :on-value-change #(set-value! %1)
+                              :className       "Field"}
+            ($ NumberField.ScrubArea {:className "ScrubArea"}
+               (when label
+                 ($ :label {:htmlFor id :className "Label"} "Amount"))
+               ($ NumberField.ScrubAreaCursor {:className "ScrubAreaCursor"}
+                  ($ cursor-grow-icon)))
+            ($ NumberField.Group {:className "Group"}
+               ($ NumberField.Decrement {:className "Decrement"} ($ minus-icon))
+               ($ NumberField.Input {:className "Input"})
+               ($ NumberField.Increment {:className "Increment"} ($ plus-icon))))))
--- src/pigeon_scoops/groceries.cljs
+++ src/pigeon_scoops/groceries.cljs
@@ -1,6 +1,7 @@
 (ns pigeon-scoops.groceries
   (:require [clojure.string :as str]
             [pigeon-scoops.api :as api]
+            [pigeon-scoops.components.number-field :refer [number-field]]
             [pigeon-scoops.context :as ctx]
             [pigeon-scoops.hooks :refer [use-token]]
             [reitit.frontend.easy :as rfe]
@@ -13,6 +14,13 @@
                                      ListItemButton
                                      ListItemText
                                      MenuItem
+                                     Paper
+                                     TableContainer
+                                     Table
+                                     TableHead
+                                     TableBody
+                                     TableRow
+                                     TableCell
                                      TextField]]))
 
 (defui grocery-list [{:keys [selected-grocery-id]}]
@@ -27,13 +35,67 @@
                           :variant   "outlined"
                           :value     filter-text
                           :on-change #(set-filter-text! (.. % -target -value))})
-            ($ List
+            ($ List {:sx (clj->js {:maxHeight "100vh"
+                                   :overflow  "auto"})}
                (for [g (sort-by :grocery/name filtered-groceries)]
                  ($ ListItemButton
                     {:key      (:grocery/id g)
                      :selected (= (:grocery/id g) selected-grocery-id)
                      :on-click #(rfe/push-state :pigeon-scoops.routes/grocery {:grocery-id (:grocery/id g)})}
                     ($ ListItemText {:primary (:grocery/name g)})))))))
+
+
+(defui grocery-unit-row [{:keys [grocery-id unit]}]
+       (let [unit-types (update-keys (->> ctx/constants-context
+                                          (uix/use-context)
+                                          :constants/unit-types
+                                          (group-by namespace))
+                                     keyword)
+
+             [source set-source!] (uix/use-state (:grocery-unit/source unit))
+             [cost set-cost!] (uix/use-state (:grocery-unit/unit-cost unit))
+             [unit-mass set-unit-mass!] (uix/use-state (or (:grocery-unit/unit-mass unit) 0))
+             [unit-mass-type set-unit-mass-type!] (uix/use-state (or (:grocery-unit/unit-mass-type unit) ""))
+             [unit-volume set-unit-volume!] (uix/use-state (or (:grocery-unit/unit-volume unit) 0))
+             [unit-volume-type set-unit-volume-type!] (uix/use-state (or (:grocery-unit/unit-volume-type unit) ""))
+             [unit-common set-unit-common!] (uix/use-state (or (:grocery-unit/unit-common unit) 0))
+             [unit-common-type set-unit-common-type!] (uix/use-state (or (:grocery-unit/unit-common-type unit) ""))]
+         ($ TableRow
+            ($ TableCell
+               ($ TextField {:value     source
+                             :on-change #(set-source! (.. % -target -value))}))
+            ($ TableCell
+               ($ number-field {:value cost :set-value! set-cost!}))
+            (for [[idx [val set-val! val-unit set-val-unit! option-key]]
+                  (map-indexed vector [[unit-mass set-unit-mass! unit-mass-type set-unit-mass-type! :mass]
+                                       [unit-volume set-unit-volume! unit-volume-type set-unit-volume-type! :volume]
+                                       [unit-common set-unit-common! unit-common-type set-unit-common-type! :common]])]
+
+              ($ TableCell {:key idx}
+                 ($ Stack {:direction "row" :spacing 1}
+                    ($ number-field {:value val :set-value! set-val!})
+                    ($ FormControl
+                       ($ Select {:value     val-unit
+                                  :on-change #(set-val-unit! (keyword (name option-key) (.. % -target -value)))}
+                          (for [o (option-key unit-types)]
+                            ($ MenuItem {:value o :key o} (name o)))))))))))
+
+
+
+(defui grocery-unit-table [{:keys [grocery-id units]}]
+       ($ TableContainer {:component Paper}
+          ($ Table
+             ($ TableHead
+                ($ TableRow
+                   ($ TableCell "Source")
+                   ($ TableCell "Cost")
+                   ($ TableCell "Mass")
+                   ($ TableCell "Volume")
+                   ($ TableCell "Common")
+                   ($ TableCell "Actions")))
+             ($ TableBody
+                (for [u units]
+                  ($ grocery-unit-row {:key (:grocery-unit/id u) :grocery-id grocery-id :unit u}))))))
 
 (defui grocery-control [{:keys [grocery]}]
        (let [{:constants/keys [departments]} (uix/use-context ctx/constants-context)
@@ -48,7 +110,7 @@
                (set-department! (:grocery/department grocery))))
            [grocery])
 
-         ($ Stack {:direction "column"}
+         ($ Stack {:direction "column" :spacing 1}
             ($ TextField {:label     "Name"
                           :value     grocery-name
                           :on-change #(set-name! (.. % -target -value))})
@@ -59,7 +121,9 @@
                           :label     "Department"
                           :on-change #(set-department! (keyword "department" (.. % -target -value)))}
                   (for [d departments]
-                    ($ MenuItem {:value d :key d} (name d))))))))
+                    ($ MenuItem {:value d :key d} (name d)))))
+            ($ grocery-unit-table {:grocery-id (:grocery/id grocery)
+                                   :units      (:grocery/units grocery)}))))
 
 (defui grocery-view [{:keys [path]}]
        (let [{:keys [grocery-id]} path
