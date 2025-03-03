626b1143c88d369ef1d35488fecbfbffbf7601f8
Gregory Meltzer <vadercows@gmail.com>
2025-03-03 11:12:16 -0500

    add mystery and description fields to recipe

--- src/pigeon_scoops/components/numbered_text_area.cljs
+++ src/pigeon_scoops/components/numbered_text_area.cljs
@@ -1,18 +1,19 @@
 (ns pigeon-scoops.components.numbered-text-area
   (:require [clojure.string :as str]
             [uix.core :refer [$ defui]]
-            ["react-textarea-autosize$default" :as TextareaAutosize]))
+            ["@mui/material" :refer [TextField]]))
 
 (defui numbered-text-area [{:keys [lines set-lines!]}]
-       ($ TextareaAutosize {:value       (str/join "\n" (map-indexed #(str (inc %1) ") " %2) lines))
-                            :on-change   #(set-lines! (let [new-lines (mapv (fn [line]
-                                                                              (str/replace line #"^\d+?\)\s*" ""))
-                                                                            (str/split-lines (.. % -target -value)))]
-                                                        (if (str/ends-with? (.. % -target -value) "\n")
-                                                          (conj new-lines "")
-                                                          new-lines)))
-                            :on-key-up   #(if (and (str/blank? (last lines))
-                                                   (= (.-key %) "Backspace"))
-                                            (set-lines! (butlast lines)))
-                            :placeholder "Instructions"}))
+       ($ TextField {:value     (str/join "\n" (map-indexed #(str (inc %1) ") " %2) lines))
+                     :on-change #(set-lines! (let [new-lines (mapv (fn [line]
+                                                                     (str/replace line #"^\d+?\)\s*" ""))
+                                                                   (str/split-lines (.. % -target -value)))]
+                                               (if (str/ends-with? (.. % -target -value) "\n")
+                                                 (conj new-lines "")
+                                                 new-lines)))
+                     :on-key-up #(if (and (str/blank? (last lines))
+                                          (= (.-key %) "Backspace"))
+                                   (set-lines! (butlast lines)))
+                     :label     "Instructions"
+                     :multiline true}))
 
