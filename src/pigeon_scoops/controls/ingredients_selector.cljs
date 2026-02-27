8b5c01a8d2a18700a009c9a8a02a9b088fc36dd4
Greg Meltzer <gregory.m.meltzer@gmail.com>
2026-02-27 17:26:43 -0500

    oooo getting form forming

--- src/pigeon_scoops/controls/ingredients_selector.cljs
+++ src/pigeon_scoops/controls/ingredients_selector.cljs
@@ -1,28 +1,49 @@
 (ns pigeon-scoops.controls.ingredients-selector
   (:require [uix.core :as uix :refer [$ defui]]
-            [antd :refer [Select Spin]]
-            [pigeon-scoops.hooks :refer [use-groceries use-recipes]]))
+            [antd :refer [Form Select Spin]]
+            [pigeon-scoops.hooks :refer [use-groceries use-recipes]]
+            [clojure.string :as str]))
 
-(defui ingredients-selector [{:keys [value on-change]}]
+(defn parse-ingredient [value]
+  (let [[type id] (.split value ":")]
+    {:type (keyword type)
+     :id id}))
+
+(defn stringify-ingredient [type id]
+  (str (name type) ":" id))
+
+(defn ingredient->option [ingredient]
+  (cond
+    (:ingredient/ingredient-recipe-id ingredient)
+    (stringify-ingredient :recipe (:ingredient/ingredient-recipe-id ingredient))
+    (:ingredient/ingredient-grocery-id ingredient)
+    (stringify-ingredient :grocery (:ingredient/ingredient-grocery-id ingredient))))
+
+(defui ingredients-selector [{:keys [form-item-name valid-ingredients]}]
   (let [{:keys [groceries] groceries-loading? :loading?} (use-groceries)
         {:keys [recipes] recipes-loading? :loading?} (use-recipes)
+        valid-ingredients (set (if (seq valid-ingredients)
+                                 valid-ingredients
+                                 [:grocery :recipe]))
         recipes (apply concat (vals recipes))
         [options set-options!] (uix/use-state [])]
     (uix/use-effect
      (fn []
        (when-not (or groceries-loading? recipes-loading?)
-         (set-options! (sort-by :label (concat
-                                        (for [g groceries]
-                                          {:value (str "grocery:" (:grocery/id g))
-                                           :label (:grocery/name g)})
-                                        (for [r recipes]
-                                          {:value (str "recipe:" (:recipe/id r))
-                                           :label (:recipe/name r)}))))))
-     [groceries recipes groceries-loading? recipes-loading?])
+         (set-options! (sort-by (comp str/lower-case :label)
+                                (concat
+                                 (when (valid-ingredients :grocery)
+                                   (for [g groceries]
+                                     {:value (stringify-ingredient :grocery (:grocery/id g))
+                                      :label (:grocery/name g)}))
+                                 (when (valid-ingredients :recipe)
+                                   (for [r recipes]
+                                     {:value (stringify-ingredient :recipe (:recipe/id r))
+                                      :label (:recipe/name r)})))))))
+     [valid-ingredients groceries recipes groceries-loading? recipes-loading?])
     (if (or groceries-loading? recipes-loading?)
       ($ Spin)
-      ($ Select {:value value
-                 :show-search (clj->js {:optionFilterProp :label})
-                 :on-change #(on-change (update (into {} (map vector [:type :id] (.split % ":")))
-                                                :type keyword))
-                 :options (clj->js options)}))))
+      ($ Form.Item {:name form-item-name
+                    :rules (clj->js [{:required true}])}
+         ($ Select {:show-search (clj->js {:optionFilterProp :label})
+                    :options (clj->js options)})))))
--- src/pigeon_scoops/recipe/forms.cljs
+++ src/pigeon_scoops/recipe/forms.cljs
@@ -1,50 +1,87 @@
 (ns pigeon-scoops.recipe.forms
-  (:require  [uix.core :as uix :refer [$ defui]]
-             [antd :refer [Button Flex Input InputNumber Form Spin Switch]]
-             ["@ant-design/icons" :refer [MinusCircleOutlined]]
-             [pigeon-scoops.controls.constants-selector :refer [constants-selector]]
-             [pigeon-scoops.controls.ingredients-selector :refer [ingredients-selector]]
-             [pigeon-scoops.hooks :refer [use-recipe]]))
+  (:require
+   ["@ant-design/icons" :refer [MinusCircleOutlined]]
+   [antd :refer [Button Flex Form Input InputNumber Space Spin Switch]]
+   [cljs.pprint :refer [pprint]]
+   [clojure.string :as str]
+   [pigeon-scoops.controls.constants-selector :refer [constants-selector]]
+   [pigeon-scoops.controls.ingredients-selector :refer [ingredient->option ingredients-selector]]
+   [pigeon-scoops.hooks :refer [use-recipe]]
+   [uix.core :as uix :refer [$ defui]]))
 
 (def TextArea (.-TextArea Input))
 
 (defn on-finish [values]
-  (prn "Submit:" values))
+  (prn "Submit:")
+  (pprint values))
+
+(defn data->form-values [data]
+  (-> data
+      (update :recipe/instructions #(str/join "\n" %))
+      (update :recipe/ingredients
+              (fn [ingredients]
+                (map #(assoc % :ingredient/ingredient-id (ingredient->option %))
+                     ingredients)))))
 
 (defui recipe-form [{:keys [recipe-id]}]
   (let [{:keys [recipe loading?]} (use-recipe recipe-id)
         [form] (Form.useForm)
+        [initial-values set-initial-values!] (uix/use-state nil)
         mystery? (Form.useWatch ":recipe/is-mystery" form)]
+
+    (uix/use-effect
+     (fn []
+       (pprint (clj->js (data->form-values recipe) :keyword-fn str))
+       (when recipe
+         (let [form-values (data->form-values recipe)]
+           (.setFieldsValue form (clj->js form-values :keyword-fn str))
+           (set-initial-values! form-values))))
+     [form recipe])
+
     (if (or loading? (not recipe))
       ($ Spin)
-      ($ Form {:form form :on-finish on-finish :initial-values (clj->js recipe :keyword-fn str)}
+      ($ Form {:form form :on-finish on-finish :style {:width "100%"} :initial-values (clj->js initial-values :keyword-fn str)}
+         ($ Form.Item {:hidden true :name (str :recipe/id)}
+            ($ Input))
          ($ Form.Item
-            ($ Button {:type "primary" :html-type "submit"}
-               (if recipe-id "Update Recipe" "Create Recipe")))
-         ($ Form.Item {:label "Name" :name ":recipe/name" :rules (clj->js [{:required true}])}
+            ($ Space
+               ($ Button {:type "primary" :html-type "submit"}
+                  (if recipe-id "Update Recipe" "Create Recipe"))
+               ($ Button {:html-type "button" :on-click #(.resetFields form)} "Reset")))
+         ($ Form.Item {:label "Name" :name (str :recipe/name) :rules (clj->js [{:required true}])}
             ($ Input))
-         ($ Form.Item {:label "Public" :name ":recipe/public"}
+         ($ Form.Item {:label "Public" :name (str :recipe/public)}
             ($ Switch))
-         ($ Form.Item {:label "Mystery Flavor" :name ":recipe/is-mystery"}
+         ($ Form.Item {:label "Mystery Flavor" :name (str :recipe/is-mystery)}
             ($ Switch))
-         ($ Form.Item {:label "Source" :name ":recipe/source"}
+         ($ Form.Item {:label "Source" :name (str :recipe/source)}
             ($ Input))
-         ($ Form.Item {:label "Description" :name ":recipe/description"}
+         ($ Form.Item {:label "Description" :name (str :recipe/description)}
             ($ TextArea))
-         (when mystery?
-           ($ Form.Item {:label "Mystery Description" :name ":recipe/mystery-description"}
-              ($ TextArea)))
+         ($ Form.Item {:hidden (not mystery?) :label "Mystery Description" :name (str :recipe/mystery-description)}
+            ($ TextArea))
          ($ Flex {:direction "row"}
-            ($ Form.Item {:label "Amount" :name ":recipe/amount" :rules (clj->js [{:required true}])}
+            ($ Form.Item {:label "Amount" :name (str :recipe/amount) :rules (clj->js [{:required true}])}
                ($ InputNumber))
-            ($ constants-selector {:form-item-name ":recipe/amount-unit" :constants-key :constants/unit-types :required? true}))
-         ($ Form.List {:name ":recipe/ingredients"}
+            ($ constants-selector {:form-item-name (str :recipe/amount-unit) :constants-key :constants/unit-types :required? true}))
+         ($ Form.Item {:label "Instructions" :name (str :recipe/instructions)}
+            ($ TextArea))
+         ($ Form.List {:name (str :recipe/ingredients)}
             (fn [fields funcs]
               ($ :div
                  (for [field fields]
                    (let [{:keys [key] field-name :name} (js->clj field :keywordize-keys true)
                          {:keys [remove]} (js->clj funcs :keywordize-keys true)]
                      ($ Flex {:direction "row" :key key}
+                        ($ Form.Item {:hidden true :name (clj->js [field-name (str :ingredient/id)])}
+                           ($ Input))
+                        ($ ingredients-selector {:form-item-name (clj->js [field-name (str :ingredient/ingredient-id)])})
+                        ($ Form.Item {:name (clj->js [field-name (str :ingredient/amount)])
+                                      :rules (clj->js [{:required true}])}
+                           ($ InputNumber))
+                        ($ constants-selector {:form-item-name (clj->js [field-name (str :ingredient/amount-unit)])
+                                               :constants-key :constants/unit-types
+                                               :required? true})
                         ($ Form.Item
                            ($ Button {:type "text" :danger true :icon ($ MinusCircleOutlined) :on-click #(remove field-name)})))))
                  ($ Form.Item
