9c4443cc700f91bca05a8a2612cdaaf541efb25e
Greg Meltzer <gregory.m.meltzer@gmail.com>
2026-03-07 23:35:39 -0500

    fix the order form

--- src/pigeon_scoops/user_order/forms.cljs
+++ src/pigeon_scoops/user_order/forms.cljs
@@ -1,11 +1,12 @@
 (ns pigeon-scoops.user-order.forms
   (:require
+   ["@ant-design/icons" :refer [MinusCircleOutlined]]
    [antd :refer [Button Flex Form Input InputNumber Space Spin]]
    [pigeon-scoops.api :refer [base-url]]
    [pigeon-scoops.controls.constants-selector :refer [constants-selector]]
-   [pigeon-scoops.controls.ingredients-selector :refer [ingredients-selector
-                                                        parse-ingredient
-                                                        ingredient->option]]
+   [pigeon-scoops.controls.ingredients-selector :refer [ingredient->option
+                                                        ingredients-selector
+                                                        parse-ingredient]]
    [pigeon-scoops.fetchers :refer [delete-fetcher! post-fetcher! put-fetcher!]]
    [pigeon-scoops.hooks :refer [invalidate-orders use-order use-token]]
    [pigeon-scoops.utils :refer [determine-ops parse-keyword stringify-keyword]]
@@ -17,49 +18,54 @@
       (update :user-order/status stringify-keyword)
       (update :user-order/items
               (fn [items]
-                (map #(-> %
-                          (update :order-item/amount-unit stringify-keyword)
-                          (update :order-item/status stringify-keyword)
-                          (assoc :order-item/recipe-id (ingredient->option
-                                                        {:recipe :order-item/recipe-id}
-                                                        %)))
-                     items)))))
+                (mapv #(-> %
+                           (update :order-item/amount-unit stringify-keyword)
+                           (update :order-item/status stringify-keyword)
+                           (assoc :order-item/ingredient-id (ingredient->option
+                                                             {:recipe :order-item/recipe-id}
+                                                             %)))
+                      items)))))
 
 (defn item-form-values->data [form-value]
   (-> form-value
       (js->clj :keywordize-keys true)
       (update :order-item/amount-unit parse-keyword)
       (update :order-item/status parse-keyword)
-      (parse-ingredient :order-item/recipe-id {:recipe :order-item/recipe-id})))
+      ((partial parse-ingredient
+                :order-item/ingredient-id
+                {:recipe :order-item/recipe-id}))))
 
 (defn order-form-values->data [form-value]
   (-> form-value
       (js->clj :keywordize-keys true)
       (update :user-order/status parse-keyword)
-      (update :user-order/items
-              (fn [items]
-                (map item-form-values->data
-                     items)))))
+      (update :user-order/items (fn [items]
+                                  (map item-form-values->data
+                                       items)))))
 
 (defn item->comparable
   ([item]
    (item->comparable item []))
   ([item additional-keys]
-   (-> item
-       item-form-values->data
-       (select-keys (concat [:order-item/status
-                             :order-item/recipe-id
-                             :order-item/amount
-                             :order-item/amount-unit]
-                            additional-keys)))))
+   (->> (-> item
+            (item-form-values->data)
+            (select-keys (concat [:order-item/amount
+                                  :order-item/amount-unit
+                                  :order-item/status
+                                  :order-item/recipe-id]
+                                 additional-keys)))
+        (remove (comp nil? second))
+        (into {}))))
 
 (defn order->comparable [order]
-  (-> order
-      order-form-values->data
-      (select-keys [:user-order/note
-                    :user-order/status
-                    :user-order/items])
-      (update :user-order/items #(map item->comparable %))))
+  (->> (-> order
+           (order-form-values->data)
+           (select-keys [:user-order/note
+                         :user-order/status
+                         :user-order/items])
+           (update :user-order/items #(map item->comparable %)))
+       (remove (comp nil? second))
+       (into {})))
 
 (defn on-finish [initial-order token values]
   (let [order (order-form-values->data values)
@@ -131,43 +137,53 @@
        (set-unsaved-changes! (apply not= (map order->comparable [order all-values]))))
      [order all-values])
 
-    (if (or loading? (not order))
+    (if (or loading? (and (not= order-id :new) (not (uuid? order-id))))
       ($ Spin)
       ($ Form {:form form
                :on-finish (partial on-finish order token all-values)
                :style {:width "100%"}
-               :initialValues (clj->js initial-values :keyword-fn stringify-keyword)}
+               :initial-values (clj->js initial-values :keyword-fn stringify-keyword)}
          ($ Space {:align "start"}
             ($ Button {:html-type "button"
                        :disabled unsaved-changes?
-                       :on-click #(rfe/push-state :pigeon-scoops.user-order.routes/orders)} "Return to Orders")
+                       :on-click #(rfe/push-state :pigeon-scoops.user-order.routes/orders)} "Return to orders")
             ($ Button {:type "primary" :html-type "submit" :disabled (not unsaved-changes?)}
-               (if (uuid? order-id) "Update Order" "Create Order"))
+               (if (uuid? order-id) "Update order" "Create order"))
             ($ Button {:html-type "button" :on-click #(.resetFields form)} "Reset")
             ($ Button {:html-type "button" :danger true :on-click (partial on-delete token order-id)} "Delete"))
-         ($ Form.Item {:label "Note"
-                       :name (stringify-keyword :user-order/note)
-                       :rules (clj->js [{:required false}])}
+         ($ Form.Item {:hidden true :name (stringify-keyword :user-order/id)}
+            ($ Input))
+         ($ Form.Item {:name (stringify-keyword :user-order/note) :label "Note" :rules (clj->js [{:required true}])}
             ($ Input))
          ($ constants-selector {:form-item-name (stringify-keyword :user-order/status)
                                 :label "Status"
                                 :constants-key :constants/order-statuses
-                                :required true})
+                                :required? true})
          ($ Form.List {:name (stringify-keyword :user-order/items)}
             (fn [fields funcs]
               ($ :div
                  (for [field fields]
                    (let [{:keys [key] field-name :name} (js->clj field :keywordize-keys true)
-                         {:keys [remove]} (js->clj funcs :kewordize-keys true)]
-                     ($ Flex {:direction "row" :key key}
-                        ($ Form.Item {:hidden true
-                                      :name (clj->js [field-name (stringify-keyword :order-item/id)])}
+                         {:keys [remove]} (js->clj funcs :keywordize-keys true)]
+                     ($ Flex {:key key :direction "row"}
+                        ($ Form.Item {:hidden true :name (clj->js [field-name (stringify-keyword :order-item/id)])}
                            ($ Input))
-                        ($ ingredients-selector {:form-item-name (clj->js [field-name (stringify-keyword :order-item/recipe-id)])
-                                                 :ingredient-keys {:recipe :order-item/recipe-id}})
-                        ($ Form.Item {:name (clj->js [field-name (stringify-keyword :order-item/amount)])
-                                      :rules (clj->js [{:required true}])}
-                           ($ InputNumber))
-                        ($ constants-selector {:form-item-name (clj->js [field-name (stringify-keyword :order-item/amount-unit)])
-                                               :constants-key :constants/unit-types
-                                               :required-true true})))))))))))
+                        ($ Flex {:vertical true}
+                           ($ Space {:align "start"}
+                              ($ ingredients-selector {:form-item-name (clj->js [field-name (stringify-keyword :order-item/ingredient-id)])
+                                                       :ingredient-keys {:recipe :order-item/recipe-id}
+                                                       :required? true})
+                              ($ Form.Item {:name (clj->js [field-name (stringify-keyword :order-item/amount)])
+                                            :rules (clj->js [{:required true}])}
+                                 ($ InputNumber {:placeholder "Amount"}))
+                              ($ constants-selector {:form-item-name (clj->js [field-name (stringify-keyword :order-item/amount-unit)])
+                                                     :constants-key :constants/unit-types
+                                                     :required? true})
+                              ($ constants-selector {:form-item-name (clj->js [field-name (stringify-keyword :order-item/status)])
+                                                     :label "Status"
+                                                     :constants-key :constants/order-statuses
+                                                     :required? true})
+                              ($ Form.Item
+                                 ($ Button {:type "text" :danger true :icon ($ MinusCircleOutlined) :on-click #(remove field-name)})))))))
+                 ($ Form.Item
+                    ($ Button {:type "dashed" :on-click (:add (js->clj funcs :keywordize-keys true))} "Add item")))))))))
