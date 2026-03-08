77fcbfa49594aef703f0874f54c5dc66d03c6a20
Greg Meltzer <gregory.m.meltzer@gmail.com>
2026-03-08 17:15:15 -0400

    make auth a button

--- src/pigeon_scoops/auth.cljs
+++ src/pigeon_scoops/auth.cljs
@@ -2,7 +2,7 @@
   (:require
    ["@ant-design/icons" :refer [UserOutlined]]
    ["@auth0/auth0-react" :refer [useAuth0]]
-   [antd :refer [Button Spin]]
+   [antd :refer [Button Popconfirm Spin]]
    [pigeon-scoops.hooks :refer [base-url use-token]]
    [uix.core :as uix :refer [$ defui]]))
 
@@ -29,14 +29,22 @@
                         (logout))))))
      [isAuthenticated token logout])
 
-    ($ Button {:type "primary"
-               :on-click (if (and isAuthenticated token)
-                           logout
-                           login)
-               :icon-placement "end"
-               :icon ($ UserOutlined {:style {:color (cond isLoading "orange"
-                                                           (and isAuthenticated token) "black"
-                                                           :else "red")}})}
-       (cond isLoading ($ Spin)
-             (and isAuthenticated token) (:name user)
-             :else "Sign In"))))
+    (if (and isAuthenticated token)
+      ($ Popconfirm {:title "Sign out"
+                     :description "Are you sure you want to sign out?"
+                     :on-confirm logout
+                     :ok-text "Yes"
+                     :cancel-text "No"}
+         ($ Button {:type "primary"
+                    :icon-placement "end"
+                    :icon ($ UserOutlined {:style {:color "black"}})}
+            (if isLoading
+              ($ Spin)
+              (:name user))))
+      ($ Button {:type "primary"
+                 :on-click login
+                 :icon-placement "end"
+                 :icon ($ UserOutlined {:style {:color (if isLoading
+                                                         "orange"
+                                                         "red")}})}
+         "Sign in"))))
