93960a4fa26a737cbb418a2f96c7bbd9a8e210a7
Greg Meltzer <gregory.m.meltzer@gmail.com>
2026-06-06 13:22:10 -0400

    bump dependencies

--- src/pigeon_scoops/storefront/views.cljs
+++
@@ -1,4 +0,0 @@
-(ns pigeon-scoops.storefront.views
-  (:require [uix.core :refer [$ defui] :as uix]
-            [pigeon-scoops.hooks :refer [use-menus
-                                         use-detailed-menus]]))
